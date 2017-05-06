5ad80876b87aad5136a92053913bbdb48ba96e34
defndaines <defndaines@gmail.com>
2017-05-06 11:09:47 -0700

    Adds quick-and-dirty display of Dijkstra distances to ASCII

--- src/meiro/ascii.clj
+++ src/meiro/ascii.clj
@@ -14,9 +14,11 @@
                    (repeat (count (first maze)) (concat horizontal-wall corner))
                    "\n"))))
 
-(defn- cell-level [cell]
-  (concat inside-cell
-          (if (some #{:east} cell) verticle-link verticle-wall)))
+(defn- cell-level
+  ([cell] (cell-level cell inside-cell))
+  ([cell inside]
+   (concat inside
+           (if (some #{:east} cell) verticle-link verticle-wall))))
 
 (defn- bottom-level [cell]
   (concat (if (some #{:south} cell) horizontal-link horizontal-wall) corner))
@@ -31,3 +33,22 @@
              (concat verticle-wall (mapcat cell-level row) "\n"
                      corner (mapcat bottom-level row) "\n"))
            maze)))
+
+(defn render-distances
+  "Render a maze with the distance values in each cell.
+  Uses base-36 to avoid spacing issues in smaller mazes."
+  [maze distances]
+  (apply str
+         (top-level maze)
+         (flatten
+           (for [row (range (count maze))]
+             (concat
+               verticle-wall
+               (for [col (range (count (first maze)))]
+                 (cell-level (get-in maze [row col])
+                             (str \space (Integer/toString (get-in distances [row col]) 36) \space)))
+               "\n"
+               corner
+               (for [col (range (count (first maze)))]
+                 (bottom-level (get-in maze [row col])))
+               "\n")))))
--- test/meiro/ascii_test.clj
+++ test/meiro/ascii_test.clj
@@ -3,6 +3,18 @@
 						[meiro.core :as m]
 						[meiro.ascii :refer :all]))
 
+(deftest cell-level-test
+  (testing "Default rendering."
+    (is (= '(\space \space \space \space)
+           (#'meiro.ascii/cell-level [:east :south])))
+    (is (= '(\space \space \space \|)
+           (#'meiro.ascii/cell-level [:south]))))
+  (testing "Pass value for inside."
+    (is (= '(\space \1 \space \space)
+           (#'meiro.ascii/cell-level [:east :south] " 1 ")))
+    (is (= '(\space \2 \space \|)
+           (#'meiro.ascii/cell-level [:south] " 2 ")))))
+
 (deftest ascii-art
 	(testing "Ensure rows and columns match."
 		(is (= "+---+---+---+\n|   |   |   |\n+---+---+---+\n|   |   |   |\n+---+---+---+\n"
@@ -14,3 +26,12 @@
 					 (render (m/link (m/init 2 2) [1 1] [1 0]))))
 		(is (= "+---+---+\n|   |   |\n+---+   +\n|   |   |\n+---+---+\n"
 					 (render (m/link (m/init 2 2) [1 1] [0 1]))))))
+
+(deftest include-distances
+  (testing "When distances are provided."
+    (let [maze [[[:east :south] [:west :east] [:west]]
+                [[:north :south] [:east] [:west :south]]
+                [[:north :east] [:west :east] [:north :west]]]
+          distances [[0 1 2] [1 6 5] [2 3 4]]]
+      (is (= "+---+---+---+\n| 0   1   2 |\n+   +---+---+\n| 1 | 6   5 |\n+   +---+   +\n| 2   3   4 |\n+---+---+---+\n"
+             (render-distances maze distances))))))
