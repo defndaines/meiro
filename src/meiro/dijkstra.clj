4cb30830a357166a3680bd4369534911dd5db28e
defndaines <defndaines@gmail.com>
2017-05-06 10:15:37 -0700

    Implements Dijkstras algorithm for calculating distances

---
+++ src/meiro/dijkstra.clj
@@ -0,0 +1,22 @@
+;;; Dijkstra's algorithm for measuring distances.
+(ns meiro.dijkstra
+  (:require [meiro.core :as m]))
+
+(defn- empty-neighbors
+  "Given a grid of distances, find neighbors of a cell which haven't been calculated yet."
+  [grid cell neighbors]
+  (filter
+    #(nil? (get-in grid %))
+    (map #(m/cell-to % cell) neighbors)))
+
+(defn distances
+  "Calculate distances to each cell relative from starting cell.
+  Assumes a perfect maze."
+  ([maze cell] (distances maze cell (m/init (count maze) (count (first maze)) nil) 0))
+  ([maze cell acc dist]
+   (if (seq cell)
+     (reduce
+       #(distances maze %2 %1 (inc dist))
+       (assoc-in acc cell dist)
+       (empty-neighbors acc cell (get-in maze cell)))
+     acc)))
---
+++ test/meiro/dijkstra_test.clj
@@ -0,0 +1,27 @@
+(ns meiro.dijkstra-test
+  (:require [clojure.test :refer :all]
+            [meiro.core :refer :all]
+            [meiro.dijkstra :refer :all]))
+
+(deftest empty-neighbor-test
+  (testing "When the grid is empty."
+    (is (= [[0 1] [2 1] [1 2] [1 0]]
+           (#'meiro.dijkstra/empty-neighbors (init 3 3 nil) [1 1] [:north :south :east :west])))
+   (is (= [[2 1] [1 2]]
+           (#'meiro.dijkstra/empty-neighbors (init 3 3 nil) [1 1] [:south :east]))))
+  (testing "When cells have been populated already"
+    (let [grid (assoc-in (init 3 3 nil) [2 1] 1)]
+      (is (= [[1 2]]
+             (#'meiro.dijkstra/empty-neighbors grid [1 1] [:south :east])))   
+      (is (= [[1 0]]
+             (#'meiro.dijkstra/empty-neighbors grid [2 0] [:north :east]))))))
+
+(deftest calculate-distances-test
+  (testing "Distance calculations."
+    (let [maze [[[:east :south] [:west :east] [:west]]
+                [[:north :south] [:east] [:west :south]]
+                [[:north :east] [:west :east] [:north :west]]]]
+      (is (= [[0 1 2] [1 6 5] [2 3 4]]
+             (distances maze [0 0])))
+      (is (= [[6 7 8] [5 0 1] [4 3 2]]
+             (distances maze [1 1]))))))
