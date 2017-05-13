fdabe9cf02fc864565f7f0c279033f7835d81e8c
defndaines <defndaines@gmail.com>
2017-05-12 20:43:57 -0700

    Implements Hunt and Kill algorithm

---
+++ src/meiro/hunt_and_kill.clj
@@ -0,0 +1,42 @@
+(ns meiro.hunt-and-kill
+  "Hunt and Kill performs a random walk against unvisited cells, then searches
+  for the first unvisited cell adjacent to a visited cell to link and continue,
+  until all cells have been visited."
+  (:require [meiro.core :as m]))
+
+;; TODO This is different from the same named function elsewhere. May rethink.
+(defn- empty-neighbors
+  [maze cell]
+  (filter #(empty? (get-in maze %)) (m/neighbors maze cell)))
+
+(defn- visited-neighbors
+  [maze cell]
+  (remove #(empty? (get-in maze %)) (m/neighbors maze cell)))
+
+(defn- hunt
+  "Find first unvisited cell with a visited neighbor."
+  [maze cells]
+  (first
+    (for [cell cells
+          neighbor (visited-neighbors maze cell)
+          :when (seq (get-in maze neighbor))]
+      [cell neighbor])))
+
+(defn create
+  "Create a random maze using the Hunt and Kill algorithm."
+  [grid]
+  (loop [maze grid
+         cell (m/random-cell maze)
+         cells (remove #{cell} (m/all-cells maze))]
+    (if (seq cells)
+      (let [unvisited (empty-neighbors maze cell)]
+        (if (seq unvisited)
+          (let [neighbor (rand-nth unvisited)]
+            (recur (m/link maze cell neighbor)
+                   neighbor
+                   (remove #{neighbor} cells)))
+          (let [[hunted visited] (hunt maze cells)]
+            (recur (m/link maze hunted visited)
+                   hunted
+                   (remove #{hunted} cells)))))
+      maze)))
---
+++ test/meiro/hunt_and_kill_test.clj
@@ -0,0 +1,20 @@
+(ns meiro.hunt-and-kill-test
+  (:require [clojure.test :refer :all]
+            [meiro.core :refer :all]
+            [meiro.hunt-and-kill :refer :all]))
+
+; (deftest walk-test
+  ; (testing "Path includes one visited cell."
+    ; (let [maze (init 8 8)]
+      ; (is (= [0 0]
+             ; (last (#'meiro.wilson/walk
+                     ; maze
+                     ; (remove #{[0 0]} (#'meiro.wilson/all-cells maze))))))
+      ; (is (= [3 4]
+             ; (last (#'meiro.wilson/walk
+                     ; maze
+                     ; (remove #{[3 4]} (#'meiro.wilson/all-cells maze)))))))))
+
+(deftest create-test
+  (testing "Ensure all cells are linked."
+    (is (every? #(not-any? empty? %) (create (init 10 12))))))
