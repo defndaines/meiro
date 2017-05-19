6506fddea1e6825373232cfd75d5f5f0ecb1d067
defndaines <defndaines@gmail.com>
2017-05-19 05:40:30 -0700

    WiP: Adds spec to core

--- src/meiro/core.clj
+++ src/meiro/core.clj
@@ -2,14 +2,21 @@
   "Core maze-generation utilities.
   Mazes are represented as a vector of vectors, which can be accessed by
   [row column]. In a fully generated maze, each cell will contain the directions
-  to open neighbor cells, e.g., [:east :south].")
+  to open neighbor cells, e.g., [:east :south]."
+  (:require [clojure.spec.alpha :as s]))
+
+;; Difference between a "cell" and "pos" (or position).
+;; A position is the [row col] index of a cell.
+(s/def ::pos (s/cat :row nat-int? :col nat-int?))
+;; A cell has links to neighbors, such as [:east :north].
+(s/def ::direction #{:north :south :east :west})
+(s/def ::cell (s/coll-of ::direction :kind vector? :distinct true))
 
 ;; Difference between "grid" and "maze"?
 ;; A grid has no links, a maze has links between the cells.
-
-;; Difference between a "cell" and "pos" (or position).
-;; A cell has links to neighbors, such as [:east :north].
-;; A position is the [row col] index of a cell.
+(s/def ::grid (s/coll-of vector? :kind vector? :min-count 1))
+(s/def ::row (s/coll-of ::cell :kind vector? :min-count 1))
+(s/def ::maze (s/coll-of ::row :kind vector? :min-count 1))
 
 ;; TODO May want to break apart code into "grid" and "maze" namespaces.
 
@@ -21,6 +28,38 @@
    (vec (repeat rows
                 (vec (repeat columns v))))))
 
+(s/fdef in?
+  :args (s/cat :grid ::grid :pos ::pos)
+  :ret boolean?)
+(defn in?
+  "Is the position within the bounds of the grid."
+  [grid pos]
+  (let [max-row (dec (count grid))
+        max-col (dec (count (first grid)))
+        [row col] pos]
+    (and
+      (<= 0 row max-row)
+      (<= 0 col max-col))))
+
+(s/fdef adjacent?
+  :args (s/cat :grid ::grid :pos-1 ::pos :pos-2 ::pos)
+  :ret boolean?)
+(defn adjacent?
+  "Are two positions adjacent.
+  Positions not within the bounds of a grid are considered not adjacent."
+  [grid pos-1 pos-2]
+  (let [[row-1 col-1] pos-1
+        [row-2 col-2] pos-2]
+    (and
+      (in? grid pos-1)
+      (in? grid pos-2)
+      (or
+        (and (= row-1 row-2) (= 1 (Math/abs (- col-1 col-2))))
+        (and (= col-1 col-2) (= 1 (Math/abs (- row-1 row-2))))))))
+
+(s/fdef direction
+  :args (s/cat :pos-1 ::pos :pos-2 ::pos)
+  :ret (s/nilable ::direction))
 (defn direction
   "Get the direction from pos-1 to pos-2.
   Assumes [0 0] is the north-west corner."
@@ -34,6 +73,9 @@
       nil)))
 
 ;; TODO Unused. Here for "completeness", but by never be used.
+(s/fdef north
+  :args (s/cat :pos ::pos)
+  :ret ::pos)
 (defn north
   "Get position to the north of a given position.
   No bounds checking, so may return an invalid position."
@@ -41,6 +83,9 @@
   (let [[row col] pos]
     [(dec row) col]))
 
+(s/fdef south
+  :args (s/cat :pos ::pos)
+  :ret ::pos)
 (defn south
   "Get position to the south of a given position.
   No bounds checking, so may return an invalid position."
@@ -48,6 +93,9 @@
   (let [[row col] pos]
     [(inc row) col]))
 
+(s/fdef east
+  :args (s/cat :pos ::pos)
+  :ret ::pos)
 (defn east
   "Get position to the east of a given position.
   No bounds checking, so may return an invalid position."
@@ -55,6 +103,9 @@
   (let [[row col] pos]
     [row (inc col)]))
 
+(s/fdef west
+  :args (s/cat :pos ::pos)
+  :ret ::pos)
 (defn west
   "Get position to the west of a given position.
   No bounds checking, so may return an invalid position."
@@ -62,6 +113,10 @@
   (let [[row col] pos]
     [row (dec col)]))
 
+;; TODO (adjacent? (:ret %) (-> % :args :pos)) ... but def of adj? requires grid
+(s/fdef pos-to
+  :args (s/cat :cardinal ::direction :pos ::pos)
+  :ret ::pos)
 (defn pos-to
   "Get neighboring position given a direction.
   No bounds checking, so may return invalid position."
@@ -80,29 +135,6 @@
   (if (seq (filter #{:west} (get-in maze pos)))
     (cons pos (path-west maze (west pos)))
     [pos]))
-
-(defn in?
-  "Is the position within the bounds of the grid."
-  [grid pos]
-  (let [max-row (dec (count grid))
-        max-col (dec (count (first grid)))
-        [row col] pos]
-    (and
-      (<= 0 row max-row)
-      (<= 0 col max-col))))
-
-(defn adjacent?
-  "Are two positions adjacent.
-  Positions not within the bounds of a grid are considered not adjacent."
-  [grid pos-1 pos-2]
-  (let [[row-1 col-1] pos-1
-        [row-2 col-2] pos-2]
-    (and
-      (in? grid pos-1)
-      (in? grid pos-2)
-      (or
-        (and (= row-1 row-2) (= 1 (Math/abs (- col-1 col-2))))
-        (and (= col-1 col-2) (= 1 (Math/abs (- row-1 row-2))))))))
 
 (defn neighbors
   "Get all potential neighbors of a position in a given grid"
