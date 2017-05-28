c006f5d5cecc48d2193c4a9ac9f42be24505bb99
defndaines <defndaines@gmail.com>
2017-05-27 17:00:15 -0700

    Adds render-masked to produce PNG with masked cells

--- src/meiro/png.clj
+++ src/meiro/png.clj
@@ -9,6 +9,15 @@
   "Cell size constant which determines cell width and height in image."
   25)
 
+(defn- draw
+  "Draw line in graphic from coordinates."
+  [graphic x y x' y']
+  (.drawLine graphic
+             (* cell-size x)
+             (* cell-size y)
+             (* cell-size x')
+             (* cell-size y')))
+
 (defn render
   "Render a maze as a PNG image."
   ([maze] (render maze "maze.png"))
@@ -24,17 +33,36 @@
      (.drawLine graphic 0 0 0 (* cell-size rows))
      (.drawLine graphic 0 0 (* cell-size cols) 0)
      (doseq [[y row] (map-indexed vector maze)]
-       (doseq [[x col] (map-indexed vector row)]
-         (when (not-any? #{:east} col)
-           (.drawLine graphic
-                      (* cell-size (inc x))
-                      (* cell-size y)
-                      (* cell-size (inc x))
-                      (* cell-size (inc y))))
-         (when (not-any? #{:south} col)
-           (.drawLine graphic
-                      (* cell-size x)
-                      (* cell-size (inc y))
-                      (* cell-size (inc x))
-                      (* cell-size (inc y))))))
+       (doseq [[x cell] (map-indexed vector row)]
+         (when (not-any? #{:east} cell)
+           (draw graphic (inc x) y (inc x) (inc y)))
+         (when (not-any? #{:south} cell)
+           (draw graphic x (inc y) (inc x) (inc y)))))
+     (ImageIO/write img "png" (File. file-name)))))
+
+(defn render-masked
+  "Render a maze as a PNG image, but not printing masked cells."
+  ([maze] (render-masked maze "maze.png"))
+  ([maze file-name]
+   (let [rows (count maze)
+         cols (count (first maze))
+         img (BufferedImage.
+               (inc (* cell-size cols))
+               (inc (* cell-size rows))
+               BufferedImage/TYPE_INT_ARGB)
+         graphic (.createGraphics img)]
+     (.setColor graphic Color/BLACK)
+     (doseq [[y row] (map-indexed vector maze)]
+       (doseq [[x cell] (map-indexed vector row)]
+         (if (not-any? #{:mask} cell)
+           (let [x+ (inc x)
+                 y+ (inc y)]
+             (when (not-any? #{:north} cell)
+               (draw graphic x y x+ y))
+             (when (not-any? #{:west} cell)
+               (draw graphic x y x y+))
+             (when (not-any? #{:east} cell)
+               (draw graphic x+ y x+ y+))
+             (when (not-any? #{:south} cell)
+               (draw graphic x y+ x+ y+))))))
      (ImageIO/write img "png" (File. file-name)))))
