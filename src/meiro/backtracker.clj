2e99466f71d60608d834f29c199cda3aeee14839
defndaines <defndaines@gmail.com>
2017-07-11 19:50:50 -0700

    Augment backtracker/create to inject empty neighbor selection
    Allows for weight/bias to be injected. For example, with 3D mazes, make
    it less likely that the maze will move north or south.

--- src/meiro/backtracker.clj
+++ src/meiro/backtracker.clj
@@ -5,21 +5,25 @@
   unvisited neighbors and walks, repeating until all cells have been linked."
   (:require [meiro.core :as m]))
 
+
 (defn create
   "Create a random maze using the Recursive Backtracker algorithm.
   If a `pos` is passed, then the random walk will begin at that position.
   `neighbor-fn` and `link-fn` allow for alternative (e.g., polar) mazes
-  to use the algorithm."
+  to use the algorithm. A `select-fn` allows for selecting which unvisited
+  neighbor to visit."
   ([grid] (create grid (m/random-pos grid)))
   ([grid pos] (create grid pos m/neighbors m/link))
   ([grid pos neighbor-fn link-fn]
+   (create grid pos neighbor-fn link-fn rand-nth))
+  ([grid pos neighbor-fn link-fn select-fn]
    (loop [maze grid
           pos pos
           stack (list pos)]
      (if (seq stack)
        (let [unvisited (m/empty-neighbors maze neighbor-fn pos)]
          (if (seq unvisited)
-           (let [neighbor (rand-nth unvisited)]
+           (let [neighbor (select-fn unvisited)]
              (recur
                (link-fn maze pos neighbor)
                neighbor
