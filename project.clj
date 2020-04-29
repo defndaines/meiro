413390afacf76060ee5619d6fff7e2c60f49d3cc
Michael Daines <953037+defndaines@users.noreply.github.com>
2020-04-28 18:29:17 -0700

    Updating dependencies to latest versions

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/defndaines/meiro"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/data.generators "0.1.2"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.generators "1.0.0"]]
   :profiles {:dev
-             {:dependencies [[org.clojure/test.check "0.9.0"]]}})
+             {:dependencies [[org.clojure/test.check "1.0.0"]]}})
